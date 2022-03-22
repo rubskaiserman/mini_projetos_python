139068ac12a361033a5101d16562d4742e42ae2a
rubskaiserman <rubskaiserman@gmail.com>
2022-03-22 13:08:07 -0300

    Fiz um jogo da forca no terminal usando python

---
+++ forca_terminal/lib/forca.py
@@ -0,0 +1,28 @@
+from string import ascii_lowercase as alfa
+import help_functions as fn
+
+def main():
+    input('Garanta que ninguém está vendo e aperte enter para inserir a palavra resposta... ')
+    ans = fn.get_answer()
+    opcoes, palpites, life = list(alfa), [], 6
+    while True:
+        fn.build_forca(ans, life, palpites)
+        if life == 0:
+            print('Derrota...')
+            print(f'A resposta era: {"".join(ans)}')
+            break
+        
+        if fn.check_endgame(palpites, ans):
+            print("VITÓRIA!!!!")  
+            break
+        res = fn.get_chute(palpites, opcoes)
+        palpites, opcoes = res[0], res[1]
+        life -= 1 if palpites[-1] not in ans else 0
+        
+
+
+if __name__ == "__main__":
+    main()
+
+
+
---
+++ forca_terminal/lib/help_functions.py
@@ -0,0 +1,51 @@
+import os
+
+def get_answer() -> list:
+    ans = list(input('Insira a palavra de resposta: ').lower())
+    os.system('cls' if os.name == 'nt' else 'clear')
+    return ans
+
+def get_chute(palpites: list, opcoes: list , repeticao=False) -> list:
+    if repeticao:
+        print('Termo inserido não incluso nas opções')
+    print(f"\nOpções: {' '.join(opcoes)}")
+    print(f'\nPalpites Dados: {" ".join(palpites)}')
+    chute = input('\nInsira seu palpite: ').lower()
+    if chute not in palpites and chute in opcoes:
+        palpites.append(chute)
+        opcoes.remove(chute)
+        return [palpites, opcoes]
+    else:
+        return get_chute(palpites, opcoes, True)
+
+def check_endgame(palpites: list, ans: list) -> bool:
+    for letter in ans:
+        if letter not in palpites:
+            return False
+
+    return True
+
+def build_forca(ans: list, life: int, palpites: list) -> None:
+    print(life)
+    print(" ___")
+    print(" |  |")
+    print(" |  O") if life < 6 else print(" |  ")
+    if life == 4:
+        print(" |  |")
+    elif life == 3:
+        print(" | /|")
+    elif life < 3:
+        print(" | /|\\")
+    else: 
+        print(" |  ")
+    if life == 1:
+        print(" |  /")
+    elif life == 0:
+        print(" |  /\\")
+    else:
+        print(" |")    
+    print('_|_', end='           ')
+
+    for letter in ans:
+        print(letter, end=' ') if letter in palpites else print('_', end=' ')
+    print()
